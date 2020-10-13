cdc56fdbc1ee64b1eec1cb0013f161209840a713
zeevosec <8338013+zeevosec@users.noreply.github.com>
2020-10-13 14:41:04 -0400

    fix: fix bug involving wrong type

--- src/profiles/molecularsequence/metadata.js
+++ src/profiles/molecularsequence/metadata.js
@@ -2,8 +2,8 @@ module.exports = {
   makeResource: (args, logger) => {
     const _count = {
       name: '_count',
-      type: 'integer',
-      fhirtype: 'integer',
+      type: 'number',
+      fhirtype: 'number',
       definition: 'https://www.hl7.org/fhir/search.html#count',
       description: 'The amount of resources to return',
     };
