38e3d53114379670b2d24eb309e98ed3346a9d2c
zeevosec <8338013+zeevosec@users.noreply.github.com>
2020-10-15 14:43:40 -0400

    test: testing github actions

--- scripts/create-gcp-cred-file.js
+++ scripts/create-gcp-cred-file.js
@@ -29,15 +29,15 @@ const {
 fs.writeFileSync(
   GCP_CRED_FILENAME,
   JSON.stringify({
-    TYPE,
-    PROJECT_ID,
-    PRIVATE_KEY_ID,
-    PRIVATE_KEY,
-    CLIENT_EMAIL,
-    CLIENT_ID,
-    AUTH_URI,
-    TOKEN_URI,
-    AUTH_PROVIDER_X509_CERT_URL,
-    CLIENT_X509_CERT_URL,
+    type: TYPE,
+    project_id: PROJECT_ID,
+    private_key_id: PRIVATE_KEY_ID,
+    private_key: PRIVATE_KEY,
+    client_email: CLIENT_EMAIL,
+    client_id: CLIENT_ID,
+    auth_uri: AUTH_URI,
+    token_uri: TOKEN_URI,
+    auth_provider_x509_cert_url: AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: CLIENT_X509_CERT_URL,
   })
 );
