3d669eee86cc3b5fc3b7532fb1d08416c309b5cc
zeevosec <8338013+zeevosec@users.noreply.github.com>
2020-10-24 13:43:55 -0400

    chore(actions): fix tests

--- fhir/src/profiles/diagnosticreport/diagnosticreport.test.js
+++ fhir/src/profiles/diagnosticreport/diagnosticreport.test.js
@@ -1,20 +1,28 @@
 const service = require('.');
 
-const TCGAService = require('../../services');
+const { TCGA } = require('../../services');
 
 describe('DiagnosticReport service tests', () => {
   let getAllSpy;
 
   beforeEach(() => {
-    getAllSpy = jest.spyOn(TCGAService.prototype, 'getAll').mockImplementation(() => [[]]);
+    getAllSpy = jest.spyOn(TCGA.prototype, 'getAll').mockImplementation(() => {
+      return {
+        data: [
+          {
+            message: 'foobar',
+          },
+        ],
+      };
+    });
   });
 
   afterEach(() => {
     jest.resetAllMocks();
   });
 
-  it('should search for DiagnosticReport', () => {
-    service.search({ base_version: 'api' }, { req: { query: {} } });
+  it('should search for DiagnosticReport', async () => {
+    await service.search({ base_version: '4_0_0' }, { req: { query: {} } });
     expect(getAllSpy).toHaveBeenCalled();
   });
 });
