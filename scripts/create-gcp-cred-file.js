0abe089a02e1a3a5911b76e850dd7bed19c05e5a
zeevosec <8338013+zeevosec@users.noreply.github.com>
2020-10-25 22:27:13 -0400

    chore(actions): update github actions

--- scripts/create-gcp-cred-file.js
+++ scripts/create-gcp-cred-file.js
@@ -31,7 +31,7 @@ fs.writeFileSync(
     type: TYPE,
     project_id: PROJECT_ID,
     private_key_id: PRIVATE_KEY_ID,
-    private_key: PRIVATE_KEY,
+    private_key: PRIVATE_KEY.replace(/\\n/g, '\n'),
     client_email: CLIENT_EMAIL,
     client_id: CLIENT_ID,
     auth_uri: AUTH_URI,
