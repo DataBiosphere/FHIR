8fee55b09b15ed9dc132a366d1a02427c9eaba01
zeevosec <8338013+zeevosec@users.noreply.github.com>
2020-11-25 13:28:18 -0500

    fix build errors

--- viewer/src/index.js
+++ viewer/src/index.js
@@ -14,9 +14,7 @@ import configureStore from './store';
 import theme from './theme';
 
 import App from './containers/App';
-import LanguageProvider from './containers/LanguageProvider';
 
-import { translationMessages } from './i18n';
 import history from './utils/history';
 
 import './styles.css';
@@ -25,54 +23,18 @@ const store = configureStore({}, history);
 
 const MOUNT_NODE = document.getElementById('root');
 
-const render = (messages) =>
-  ReactDOM.render(
-    <Provider store={store}>
-      <LanguageProvider messages={messages}>
-        <ConnectedRouter history={history}>
-          <HelmetProvider>
-            <ThemeProvider theme={theme}>
-              <CssBaseline />
-              <Switch>
-                <Route path="/" component={App} />
-              </Switch>
-            </ThemeProvider>
-          </HelmetProvider>
-        </ConnectedRouter>
-      </LanguageProvider>
-    </Provider>,
-    MOUNT_NODE
-  );
-
-if (module.hot) {
-  // Hot reloadable React components and translation json files
-  // modules.hot.accept does not accept dynamic dependencies,
-  // have to be constants at compile-time
-  module.hot.accept(['./i18n', './containers/App'], () => {
-    ReactDOM.unmountComponentAtNode(MOUNT_NODE);
-    render(translationMessages);
-  });
-}
-
-// Chunked polyfill for browsers without Intl support
-if (!window.Intl) {
-  new Promise((resolve) => {
-    resolve(import('intl'));
-  })
-    .then(() =>
-      Promise.all([import('intl/locale-data/jsonp/en.js'), import('intl/locale-data/jsonp/de.js')])
-    )
-    .then(() => render(translationMessages))
-    .catch((err) => {
-      throw err;
-    });
-} else {
-  render(translationMessages);
-}
-
-// Install ServiceWorker and AppCache in the end since
-// it's not most important operation and if main code fails,
-// we do not want it installed
-if (process.env.NODE_ENV === 'production') {
-  require('offline-plugin/runtime').install(); // eslint-disable-line global-require
-}
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <HelmetProvider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Switch>
+            <Route path="/" component={App} />
+          </Switch>
+        </ThemeProvider>
+      </HelmetProvider>
+    </ConnectedRouter>
+  </Provider>,
+  MOUNT_NODE
+);
