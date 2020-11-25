9474e6105366381ba2ae80a54c46ca377fcb47a6
zeevosec <8338013+zeevosec@users.noreply.github.com>
2020-11-25 13:51:27 -0500

    add healthcheck endpoint on root

--- fhir/src/index.js
+++ fhir/src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const express = require('express');
 const FHIRServer = require('@asymmetrik/node-fhir-server-core');
 const { loggers } = require('@asymmetrik/node-fhir-server-core');
 
@@ -10,11 +11,20 @@ const { PORT = 3000 } = process.env;
 
 const logger = loggers.get();
 
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ status: 'up' });
+});
+
 const main = () => {
-  const server = FHIRServer.initialize({
-    ...fhirServerConfig,
-    statementGenerator: generateStatement,
-  });
+  const server = FHIRServer.initialize(
+    {
+      ...fhirServerConfig,
+      statementGenerator: generateStatement,
+    },
+    app
+  );
   server.listen(PORT, () => logger.info(`Broad FHIR listening on port ${PORT}`));
 };
 
