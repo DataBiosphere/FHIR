a721d342e01ae8f2906ac30628dcda926fe7b9f1
Luan <ltran@asymmetrik.com>
2021-01-14 11:35:28 -0500

    feat: display ResearchStudy

--- viewer/src/containers/Search/mappings/index.js
+++ viewer/src/containers/Search/mappings/index.js
@@ -1,4 +1,5 @@
 import * as DiagnosticReport from './diagnosticreport';
 import * as Observation from './observation';
+import * as ResearchStudy from './researchstudy';
 
-export default { DiagnosticReport, Observation };
+export default { DiagnosticReport, Observation, ResearchStudy };
---
+++ viewer/src/containers/Search/mappings/researchstudy.js
@@ -0,0 +1,2 @@
+export const columns = ['Study', 'Title', 'status'];
+export const renderers = ['id', 'title', 'status'];
