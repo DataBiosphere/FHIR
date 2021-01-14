44a06149e433b40a2a36382bccd43af18041e4d4
Luan <ltran@asymmetrik.com>
2021-01-14 11:52:23 -0500

    feat: display Specimen (placeholder)

--- viewer/src/containers/Search/mappings/index.js
+++ viewer/src/containers/Search/mappings/index.js
@@ -1,5 +1,6 @@
 import * as DiagnosticReport from './diagnosticreport';
 import * as Observation from './observation';
+import * as Specimen from './specimen';
 import * as ResearchStudy from './researchstudy';
 
-export default { DiagnosticReport, Observation, ResearchStudy };
+export default { DiagnosticReport, Observation, Specimen, ResearchStudy };
