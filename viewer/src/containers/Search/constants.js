be1e0c274bb5d398d66cb8a614071cfe59e1d887
Luan <ltran@asymmetrik.com>
2021-01-20 17:19:32 -0500

    wip(viewer): adding initial constants

--- viewer/src/containers/Search/constants.js
+++ viewer/src/containers/Search/constants.js
@@ -9,3 +9,8 @@ export const GET_BUNDLE = 'app/Search/GET_BUNDLE';
 export const GET_BUNDLE_REQUEST = 'app/Search/GET_BUNDLE_REQUEST';
 export const GET_BUNDLE_SUCCESS = 'app/Search/GET_BUNDLE_SUCCESS';
 export const GET_BUNDLE_ERROR = 'app/Search/GET_BUNDLE_ERROR';
+
+export const CASE_ID = 'id';
+export const SUBJECT = 'subject.reference';
+export const STUDY = 'extension[0].valueReference.reference';
+export const STATUS = 'status';
