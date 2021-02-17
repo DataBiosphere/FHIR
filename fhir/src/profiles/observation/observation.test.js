2629fe166cef7ecbf2876c863b81c28bda976213
Luan <ltran@asymmetrik.com>
2021-02-17 13:22:52 -0500

    chore: updated observation tests

--- fhir/src/profiles/observation/observation.test.js
+++ fhir/src/profiles/observation/observation.test.js
@@ -1,5 +1,6 @@
 const service = require('.');
 
+const { TCGA_SOURCE, ANVIL_SOURCE } = require('../../utils');
 const { TCGA, ANVIL } = require('../../services');
 
 describe('Observation service tests', () => {
@@ -55,24 +56,32 @@ describe('Observation service tests', () => {
   });
 
   it('should search only TCGA data', async () => {
-    await service.search({ base_version: '4_0_0' }, { req: { query: { _source: 'tcga' } } });
+    await service.search({ base_version: '4_0_0' }, { req: { query: { _source: TCGA_SOURCE } } });
     expect(getAllTCGASpy).toHaveBeenCalled();
   });
 
   it('should search only ANVIL data', async () => {
-    await service.search({ base_version: '4_0_0' }, { req: { query: { _source: 'anvil' } } });
+    await service.search({ base_version: '4_0_0' }, { req: { query: { _source: ANVIL_SOURCE } } });
     expect(getAllANVILSpy).toHaveBeenCalled();
   });
 
   it('should search for Observation by TCGA ID', async () => {
-    await service.searchById({ base_version: '4_0_0' }, { req: { params: { id: 'TCGA-HNSC' } } });
+    await service.searchById(
+      { base_version: '4_0_0' },
+      { req: { params: { id: 'TCGA-HNSC' }, query: { _source: TCGA_SOURCE } } }
+    );
     expect(getByTCGAIdSpy).toHaveBeenCalled();
   });
 
   it('should search for Observation by ANVIL ID', async () => {
     await service.searchById(
       { base_version: '4_0_0' },
-      { req: { params: { id: 'AnVIL_CMG_Broad_Blood_Gazda_WGS' } } }
+      {
+        req: {
+          params: { id: 'AnVIL_CMG_Broad_Blood_Gazda_WGS' },
+          query: { _source: ANVIL_SOURCE },
+        },
+      }
     );
     expect(getByANVILIdSpy).toHaveBeenCalled();
   });
