62a4e7e0569ec9197face54ae0c1d881fb8f107e
Luan <ltran@asymmetrik.com>
2021-03-01 17:07:58 -0500

    fix: updated controller tests

--- anvil-api/src/anvil/controller.test.js
+++ anvil-api/src/anvil/controller.test.js
@@ -1,12 +1,12 @@
 jest.mock('./service', () => ({
-  getAllWorkspaces: jest.fn().mockImplementation(() => [[{ id: 'test' }], 100]),
-  getWorkspaceById: jest.fn().mockImplementation(() => ({ id: 'test' })),
-  getAllSamples: jest.fn().mockImplementation(() => [[{ id: 'test' }], 100]),
-  getSampleById: jest.fn().mockImplementation(() => ({ id: 'test' })),
-  getAllSubjects: jest.fn().mockImplementation(() => [[{ id: 'test' }], 100]),
-  getSubjectById: jest.fn().mockImplementation(() => ({ id: 'test' })),
-  getAllObservations: jest.fn().mockImplementation(() => [[{ id: 'test' }], 100]),
-  getObservationById: jest.fn().mockImplementation(() => ({ id: 'test' })),
+  getAllWorkspaces: jest.fn().mockImplementation(() => [[{ id: 'workspace' }], 1]),
+  getWorkspaceById: jest.fn().mockImplementation(() => ({ id: 'workspaceid' })),
+  getAllSamples: jest.fn().mockImplementation(() => [[{ id: 'sample' }], 2]),
+  getSampleById: jest.fn().mockImplementation(() => ({ id: 'sampleid' })),
+  getAllSubjects: jest.fn().mockImplementation(() => [[{ id: 'subject' }], 3]),
+  getSubjectById: jest.fn().mockImplementation(() => ({ id: 'subjectid' })),
+  getAllObservations: jest.fn().mockImplementation(() => [[{ id: 'observation' }], 4]),
+  getObservationById: jest.fn().mockImplementation(() => ({ id: 'observationid' })),
 }));
 
 const controller = require('./controller');
@@ -25,7 +25,7 @@ describe('ANVIL controller tests', () => {
     };
 
     await controller.getAllWorkspaces(mockReq, mockRes);
-    expect(mockRes.json.mock.calls[0][0]).toEqual({ count: 100, results: [{ id: 'test' }] });
+    expect(mockRes.json.mock.calls[0][0]).toEqual({ count: 1, results: [{ id: 'workspace' }] });
   });
 
   it('should get Workspace data by ID', async () => {
@@ -40,7 +40,7 @@ describe('ANVIL controller tests', () => {
     };
 
     await controller.getWorkspaceById(mockReq, mockRes);
-    expect(mockRes.json.mock.calls[0][0]).toEqual({ id: 'test' });
+    expect(mockRes.json.mock.calls[0][0]).toEqual({ id: 'workspaceid' });
   });
 
   it('should get all Sample data', async () => {
@@ -56,7 +56,7 @@ describe('ANVIL controller tests', () => {
     };
 
     await controller.getAllSamples(mockReq, mockRes);
-    expect(mockRes.json.mock.calls[0][0]).toEqual({ count: 100, results: [{ id: 'test' }] });
+    expect(mockRes.json.mock.calls[0][0]).toEqual({ count: 2, results: [{ id: 'sample' }] });
   });
   it('should get all Sample data with Workspace params', async () => {
     const mockRes = {
@@ -74,7 +74,7 @@ describe('ANVIL controller tests', () => {
     };
 
     await controller.getAllSamples(mockReq, mockRes);
-    expect(mockRes.json.mock.calls[0][0]).toEqual({ count: 100, results: [{ id: 'test' }] });
+    expect(mockRes.json.mock.calls[0][0]).toEqual({ count: 2, results: [{ id: 'sample' }] });
   });
 
   it('should get Sample data by ID', async () => {
@@ -89,7 +89,7 @@ describe('ANVIL controller tests', () => {
     };
 
     await controller.getSampleById(mockReq, mockRes);
-    expect(mockRes.json.mock.calls[0][0]).toEqual({ id: 'test' });
+    expect(mockRes.json.mock.calls[0][0]).toEqual({ id: 'sampleid' });
   });
   it('should get Sample data by Workspace ID', async () => {
     const mockRes = {
@@ -104,7 +104,7 @@ describe('ANVIL controller tests', () => {
     };
 
     await controller.getSampleById(mockReq, mockRes);
-    expect(mockRes.json.mock.calls[0][0]).toEqual({ id: 'test' });
+    expect(mockRes.json.mock.calls[0][0]).toEqual({ id: 'sampleid' });
   });
 
   it('should get all Subject data', async () => {
@@ -120,7 +120,7 @@ describe('ANVIL controller tests', () => {
     };
 
     await controller.getAllSubjects(mockReq, mockRes);
-    expect(mockRes.json.mock.calls[0][0]).toEqual({ count: 100, results: [{ id: 'test' }] });
+    expect(mockRes.json.mock.calls[0][0]).toEqual({ count: 3, results: [{ id: 'subject' }] });
   });
   it('should get all Subject data with Workspace params', async () => {
     const mockRes = {
@@ -138,7 +138,7 @@ describe('ANVIL controller tests', () => {
     };
 
     await controller.getAllSubjects(mockReq, mockRes);
-    expect(mockRes.json.mock.calls[0][0]).toEqual({ count: 100, results: [{ id: 'test' }] });
+    expect(mockRes.json.mock.calls[0][0]).toEqual({ count: 3, results: [{ id: 'subject' }] });
   });
 
   it('should get Subject data by ID', async () => {
@@ -153,7 +153,7 @@ describe('ANVIL controller tests', () => {
     };
 
     await controller.getSubjectById(mockReq, mockRes);
-    expect(mockRes.json.mock.calls[0][0]).toEqual({ id: 'test' });
+    expect(mockRes.json.mock.calls[0][0]).toEqual({ id: 'subjectid' });
   });
   it('should get Subject data by Workspace ID', async () => {
     const mockRes = {
@@ -168,6 +168,70 @@ describe('ANVIL controller tests', () => {
     };
 
     await controller.getSubjectById(mockReq, mockRes);
-    expect(mockRes.json.mock.calls[0][0]).toEqual({ id: 'test' });
+    expect(mockRes.json.mock.calls[0][0]).toEqual({ id: 'subjectid' });
+  });
+
+  it('should get all Observation data', async () => {
+    const mockRes = {
+      json: jest.fn(),
+    };
+
+    const mockReq = {
+      query: {
+        page: 1,
+        pageSize: 10,
+      },
+    };
+
+    await controller.getAllObservations(mockReq, mockRes);
+    expect(mockRes.json.mock.calls[0][0]).toEqual({ count: 4, results: [{ id: 'observation' }] });
+  });
+  it('should get all Observation data with Workspace params', async () => {
+    const mockRes = {
+      json: jest.fn(),
+    };
+
+    const mockReq = {
+      params: {
+        workspace: 'foobar',
+      },
+      query: {
+        page: 1,
+        pageSize: 10,
+      },
+    };
+
+    await controller.getAllObservations(mockReq, mockRes);
+    expect(mockRes.json.mock.calls[0][0]).toEqual({ count: 4, results: [{ id: 'observation' }] });
+  });
+
+  it('should get Observation data by ID', async () => {
+    const mockRes = {
+      json: jest.fn(),
+    };
+
+    const mockReq = {
+      params: {
+        id: 'foobar',
+      },
+    };
+
+    await controller.getObservationById(mockReq, mockRes);
+    expect(mockRes.json.mock.calls[0][0]).toEqual({ id: 'observationid' });
+  });
+  it('should get Observation data by Workspace ID', async () => {
+    const mockRes = {
+      json: jest.fn(),
+    };
+
+    const mockReq = {
+      params: {
+        workspace: 'foo',
+        id: 'bar',
+      },
+    };
+
+    await controller.getObservationById(mockReq, mockRes);
+    expect(mockRes.json.mock.calls[0][0]).toEqual({ id: 'observationid' });
   });
 });
